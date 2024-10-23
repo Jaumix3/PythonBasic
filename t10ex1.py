1e98b276e3e9305bde7a916870b378faa5e6db0e
Jaumix3 <topotamadre1811@gmail.com>
2024-10-23 12:26:43 +0200

    Segon exercici

---
+++ t10ex1.py
@@ -0,0 +1,4 @@
+
+nom = input("Qui és el teu nom? ")
+cognom = input("I el teu cognom? ")
+print("Hola {} {}".format(nom, cognom))
---
+++ t10ex2.py
@@ -0,0 +1,5 @@
+edat = int(input("Quina edat tens? "))
+if edat < 18:
+    print("Tes menor d'edat ")
+else:
+    print("Ets Major d'edat ")
