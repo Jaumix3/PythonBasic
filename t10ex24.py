9537af5fc3f2fea32793a8379b34d758e40d3cd0
Jaumix3 <topotamadre1811@gmail.com>
2024-12-04 13:30:19 +0100

    jaja

--- t10ex24.py
+++ t10ex24.py
@@ -1,17 +1,17 @@
 def llegir_llista():
-    l=[]
-    a="a"
-    while a!=".":
-        a=input("Introdueix un número: ")
-        if a!=".":
-            l.append(int(a))
-    return l
+        l=[]
+        a="a"
+        while a!=".":
+            a=input("Introdueix un número: ")
+            if a!=".":
+                l.append(int(a))
+        return l
 def crear_punts(l):
-    s="."
-    for e in l:
-        print("{} \n".format(s*e))
         s="."
-#Programa principal
+        for e in l:
+            print("{} \n".format(s*e))
+            s="."
+    #Programa principal
 a = llegir_llista()
 b=llegir_llista()
 crear_punts(a)
---
+++ t10ex27.py
@@ -0,0 +1,11 @@
+def llegir_llista():
+    l=[]
+    a="a"
+    while a!=".":
+        a=input("Introdueix una paraula: ")
+        if a!=".":
+            l.append(a)
+    return l
+
+def filtrar_paraules(l):
+    a=l[]
