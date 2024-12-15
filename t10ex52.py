27cbcd72b6ca21b937edc4f64d2c1ddf2b7ddc38
Jaumix3 <151159839+Jaumix3@users.noreply.github.com>
2024-12-15 13:00:43 +0100

    yay

---
+++ t10ex52.py
@@ -0,0 +1,27 @@
+def llegir_llista():
+   l=[]
+   a="a"
+   while a!=".":
+       a=input("Introdueix una paraula: ")
+       if a!=".":
+           l.append(a)
+   return l
+
+
+def index_paraula(l, paraula):
+   if paraula not in l:
+       return -1
+   else:
+       for i,e in enumerate(l):
+           if e==paraula:
+               return i
+
+
+#Programa principal
+l= llegir_llista()
+p = input("Dir quina paraula de la llista vols cercar: ")
+n=index_paraula(l,p)
+if n>=0:
+   print("La paraula {} està en la posició {}".format(p, n+1))
+else:
+   print("La paraula no està dins la llista")
