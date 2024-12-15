5e48c302a3b66f8beed3bbe336d237e7fb59ba69
Jaumix3 <151159839+Jaumix3@users.noreply.github.com>
2024-12-15 13:00:16 +0100

    Update t10ex50.py

---
+++ t10ex50.py
@@ -0,0 +1,19 @@
+def llegir_llista():
+   l=[]
+   a="a"
+   while a!=".":
+       a=input("Introdueix un número: ")
+       if a!=".":
+           l.append(int(a))
+   return l
+
+
+def elimina_duplicats(l):
+   s = set(l)
+   return list(s)
+
+
+#programa principal
+l=llegir_llista()
+r=elimina_duplicats(l)
+print("La llista {} queda així {}".format(l,r))
