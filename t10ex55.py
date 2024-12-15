1e7b4a05b4b45c67f7d35e99a82c6e87a77b8a07
Jaumix3 <151159839+Jaumix3@users.noreply.github.com>
2024-12-15 13:01:29 +0100

    yay

---
+++ t10ex55.py
@@ -0,0 +1,15 @@
+x=int(input("Escriu el menor: "))
+y=int(input("Escriu el major: "))
+suma=0
+for i in range(x,y+1,1):
+   suma+=i
+print(suma)
+
+
+x=int(input("Escriu el menor: "))
+y=int(input("Escriu el major: "))
+suma=0
+for i in range(x,y+1,1):
+   if i%2==0:
+       suma+=i
+print(suma)
