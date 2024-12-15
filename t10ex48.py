77cf7ae680d29cbcd56373bb91b744b9ce45a3c0
Jaumix3 <151159839+Jaumix3@users.noreply.github.com>
2024-12-15 12:59:41 +0100

    yay

---
+++ t10ex48.py
@@ -0,0 +1,22 @@
+def llegir_llista():
+  
+   l=[]
+   a="a"
+   while a!=".":
+       a=input("Introdueix un número: ")
+       if a!=".":
+           l.append(int(a))
+   return l
+
+
+def hi_ha_duplicats(l):
+   s = set(l)
+   if len(l)==len(s):
+       print("No hi ha duplicats")
+   else:
+       print("Hi ha duplicats")
+
+
+#Programa Principal
+l= llegir_llista()
+hi_ha_duplicats(l)
